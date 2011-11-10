112606c9c64ae30826f8f2e355632e7b1f7b5b7b
meleyal <william.meleyal@gmail.com>
2011-11-10 14:09:44 +0100

    move helpers to app

--- lib/serialize/engine.rb
+++ lib/serialize/engine.rb
@@ -1,9 +1,6 @@
 module Typus
   module Serialize
     class Engine < Rails::Engine
-      def initialize
-        ActionView::Base.send(:include, Typus::Serialize::ApplicationHelper)
-      end
     end
   end
 end
--- lib/typus_serialize.rb
+++ lib/typus_serialize.rb
@@ -2,10 +2,9 @@ module Typus
   module Serialize
     if defined?(Rails)
       require 'jquery-rails'
-      require 'rails-backbone'
+      require 'backbone-on-rails'
       require 'eco'
       require 'serialize/engine'
-      require 'serialize/helpers'
       require 'serialize/version'
     end
   end
